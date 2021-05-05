59d5ecc352a646c136d99decda3a5d9700479dd1
masarukono <masarukono@gmail.com>
2021-05-05 20:28:04 +0900

    from 1 to 5

--- html/image-gallery/main.js
+++ html/image-gallery/main.js
@@ -6,14 +6,20 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-function imgPick(i) {
+function imgNo(i) {
     return 'images/pic' + i.string() + '.jpg';
 }
 
 for (let i = 1; i < 6; i++) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', imgPick(i));
+    newImage.setAttribute('src', imgNo(i));
     thumbBar.appendChild(newImage);
+}
+
+thumbBar.onclick = function() {
+    const src = getAttribute('src').split('.');
+    const i = Number(src[0][src[0].length-1]);
+    displayedImage.setAttribute('src', imgNo(i));
 }
 
 /* Wiring up the Darken/Lighten button */
