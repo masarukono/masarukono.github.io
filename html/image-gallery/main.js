16d3829b1dae9792de7473d666f0609b2c2aab0a
masarukono <masarukono@gmail.com>
2021-05-05 20:28:53 +0900

    from 1 to 5

--- html/image-gallery/main.js
+++ html/image-gallery/main.js
@@ -15,11 +15,11 @@ for (let i = 1; i < 6; i++) {
     newImage.setAttribute('src', imgNo(i));
     thumbBar.appendChild(newImage);
 }
-
+/*
 thumbBar.onclick = function() {
     const src = getAttribute('src').split('.');
     const i = Number(src[0][src[0].length-1]);
     displayedImage.setAttribute('src', imgNo(i));
 }
-
+*/
 /* Wiring up the Darken/Lighten button */
